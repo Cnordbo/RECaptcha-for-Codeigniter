1daf7ead072db01bdbd136578c3bbc0bcf3fbf06
unknown <Irwandi@Guest-PC02.(none)>
2013-05-14 16:57:40 +0700

    Add To Folder

---
+++ config/recaptcha.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * File     : recaptcha.php
+ * Created  : May 14, 2013 | 4:22:40 PM
+ * 
+ * Author   : Andi Irwandi Langgara <irwandi@ourbluecode.com>
+ */
+
+$config['public_key']   = 'YOUR PUBLIC KEY';
+$config['private_key']  = 'YOUR PRIVATE KEY';
+
+?>
