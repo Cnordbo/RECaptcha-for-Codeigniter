32863a201b3de4d84622a13fd0b0b8b0a2baf32d
ctepeo <i@ctepeo.by>
2014-03-22 08:23:23 +0300

    Added all RecaptchaOptions configuration, not only 'theme'

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -10,7 +10,15 @@
 
 $config['public_key']   = 'YOUR PUBLIC KEY';
 $config['private_key']  = 'YOUR PRIVATE KEY';
-// Set Recaptcha theme, default red (red/white/blackglass/clean)
-$config['recaptcha_theme']  = 'red';
+
+// Set Recaptcha options
+// Reference at https://developers.google.com/recaptcha/docs/customization
+$config['recaptcha_options']  = array(
+    'theme'=>'red', // red/white/blackglass/clean
+    'lang' => 'en' // en/nl/fl/de/pt/ru/es/tr
+    //  'custom_translations' - Use this to specify custom translations of reCAPTCHA strings.
+    //  'custom_theme_widget' - When using custom theming, this is a div element which contains the widget. See the custom theming section for how to use this.
+    //  'tabindex' - Sets a tabindex for the reCAPTCHA text box. If other elements in the form use a tabindex, this should be set so that navigation is easier for the user
+);
 
 ?>
